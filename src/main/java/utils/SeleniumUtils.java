583b6c6f9a9a24c6ba78cf34fcfa643d08d79cc4
Yuri Rodrigues Tolla da Silva <yuritolla@gmail.com>
2020-02-25 21:04:04 -0300

    Update SeleniumUtils.java

--- src/main/java/utils/SeleniumUtils.java
+++ src/main/java/utils/SeleniumUtils.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.WebDriver;
 public class SeleniumUtils {
 
   public static WebDriver driver;
-
-
+  
   public void wait(int tempo) throws InterruptedException {
     tempo = tempo * 1000;
     sleep(tempo);
